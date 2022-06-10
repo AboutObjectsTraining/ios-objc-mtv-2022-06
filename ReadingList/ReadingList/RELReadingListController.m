8c95a311860a4f47201f8abdf66a34c355af2a0b
Jonathan Lehr <jonathan@aboutobjects.com>
2022-06-10 14:13:32 -0400

    Initial version of ReadingList app

---
+++ ReadingList/ReadingList/RELReadingListController.m
@@ -0,0 +1,18 @@
+// Copyright (C) 2022 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "RELReadingListController.h"
+
+@implementation RELReadingListController
+
+// MARK: - Unwind segues
+
+- (IBAction)done:(UIStoryboardSegue *)unwindSegue {
+    // TODO: Persist changes and refresh UI
+}
+
+- (IBAction)cancel:(UIStoryboardSegue *)unwindSegue {
+    
+}
+
+@end
---
+++ ReadingList/ReadingList/RELSceneDelegate.m
@@ -0,0 +1,12 @@
+// Copyright (C) 2022 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "RELSceneDelegate.h"
+
+@interface RELSceneDelegate ()
+
+@end
+
+@implementation RELSceneDelegate
+
+@end
